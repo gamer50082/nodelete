9c4e0c95c1c6fa3796b506c87b033cfe4aa96a0f
Jamie_ <volcanomonster07@gmail.com>
2021-10-22 15:29:01 +0100

    Fixing db file

--- db.js
+++ db.js
@@ -2,56 +2,38 @@ const settings = require("./settings.json");
 const chalk = require("chalk");
 
 module.exports = async function (app) {
-    if (settings.database.mysql.enabled === true) {
-        const mysql = require("mysql");
+    const session = require("express-session");
+    const MongoDBStore = require("connect-mongodb-session")(session);
+    const {
+        MongoClient
+    } = require("mongodb");
 
-        const connection = mysql.createConnection({
-            host: "localhost",
-            user: "me",
-            password: "secret",
-            database: "my_db",
-        });
+    const store = new MongoDBStore({
+        uri: settings.database.url,
+        databaseName: settings.database.dbname,
+        collection: "sessions",
+    });
 
-    } else if (settings.database.json.enabled === true) {
+    app.use(require('express-session')({
+        secret: settings.website.secret,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        },
+        store: store,
+        resave: true,
+        saveUninitialized: true,
+    }));
 
-    } else if (settings.database.mongodb.enabled === true) {
-        const session = require("express-session");
-        const MongoDBStore = require("connect-mongodb-session")(session);
-        const { MongoClient } = require("mongodb");
+    const client = new MongoClient(settings.database.url);
 
-        const store = new MongoDBStore({
-            uri: settings.database.mongodb.url,
-            databaseName: settings.database.mongodb.dbname,
-            collection: "sessions",
-        });
-
-        app.use(require('express-session')({
-            secret: settings.website.secret,
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-            },
-            store: store,
-            resave: true,
-            saveUninitialized: true,
-        })
-        );
-
-        const client = new MongoClient(settings.database.mongodb.url);
-
-        await client.connect();
-        console.log(chalk.green('[DATABASE] Connected to the database'));
+    await client.connect();
+    console.log(chalk.green('[DATABASE] Connected to the database'));
 
 
-        // Catch errors
-        store.on("error", function (error) {
-            console.log(error);
-        });
+    // Catch errors
+    store.on("error", function (error) {
+        console.log(error);
+    });
 
-        return client
-
-    } else if (settings.database.sqlite.enabled === true) {
-
-    } else if (settings.database.mariadb.enabled === true) {
-
-    }
+    return client
 }
